55c60ca56e8d8ba1c414d90fda1195cb3d656e91
kucanon <kucanon@users.noreply.github.com>
2014-03-20 22:10:28 +0100

    Add method for inserting button bar

--- kurahenPremium.user.js
+++ kurahenPremium.user.js
@@ -11,6 +11,19 @@
 (function () {
 	'use strict';
 
+	var bbCodes = ['b', 'i', 'u', 's', 'small', 'code', 'spoiler'];
+	var wordfilters = [
+		['#nowocioty', 'STAROCIOTY PAMIĘTAJĄ'],
+		['#gimbo', 'xD'],
+		['#penis', 'pisiorek'],
+		['#wagina', 'cipuszka'],
+		['#m__b', 'groźny WYKOPEK wykryty'],
+		['#Lasoupeauxchoux', 'kapuśniaczek'],
+		['#homoś', 'pedał'],
+		['#korwinkrulempolski', 'kongres nowej prawicy'],
+		['#1%', 'groźny LEWAK wykryty']
+	];
+
 	var KurahenPremium = function () {
 	};
 
@@ -32,4 +45,80 @@
 		document.body.style.fontFamily = 'Roboto, "Helvetica Neue", Helvetica, Arial, sans-serif';
 	};
 
+	KurahenPremium.prototype.insertButtonBar = function () {
+		var postForm = document.getElementById('postform');
+		var textarea = document.querySelector("#postform textarea");
+		var buttonBar = document.createElement('div');
+		buttonBar.style.textAlign = 'center';
+
+		this.insertTextFormattingButtons(textarea, buttonBar);
+		this.insertWordfilterList(textarea, buttonBar);
+
+		postForm.insertBefore(buttonBar, postForm.firstChild);
+	};
+
+	/**
+	 * @private
+	 */
+	KurahenPremium.prototype.insertTextFormattingButtons = function (textarea, buttonBar) {
+		var onButtonClick = function () {
+			var startTag = '[' + this.value + ']';
+			var endTag = '[/' + this.value + ']';
+
+			var textBeforeSelection = textarea.value.substring(0, textarea.selectionStart);
+			var selectedText = textarea.value.substring(textarea.selectionStart, textarea.selectionEnd);
+			var textAfterSelection = textarea.value.substring(textarea.selectionEnd, textarea.value.length);
+
+			textarea.value = textBeforeSelection + startTag + selectedText + endTag + textAfterSelection;
+
+			textarea.focus();
+			textarea.selectionStart += startTag.length;
+			textarea.selectionEnd = textarea.selectionStart + selectedText.length;
+		};
+
+		for (var i = 0; i < bbCodes.length; i++) {
+			var button = document.createElement('input');
+			button.type = 'button';
+			button.value = bbCodes[i];
+			button.addEventListener('click', onButtonClick, false);
+			buttonBar.appendChild(button);
+		}
+	};
+
+	/**
+	 * @private
+	 */
+	KurahenPremium.prototype.insertWordfilterList = function (textarea, buttonBar) {
+		var wordfiltersSelect = document.createElement('select');
+
+		var defaultOption = document.createElement('option');
+		defaultOption.value = '';
+		defaultOption.disabled = true;
+		defaultOption.selected = true;
+		defaultOption.textContent = 'WORDFILTRY';
+		wordfiltersSelect.appendChild(defaultOption);
+
+		wordfiltersSelect.addEventListener('change', function () {
+			var textToInsert = this.options[this.selectedIndex].value;
+			var textBeforeEndOfSelection = textarea.value.substring(0, textarea.selectionEnd);
+			var textAfterEndOfSelection = textarea.value.substring(textarea.selectionEnd, textarea.value.length);
+
+			textarea.value = textBeforeEndOfSelection + textToInsert + textAfterEndOfSelection;
+
+			textarea.focus();
+			textarea.selectionStart = textBeforeEndOfSelection.length + textToInsert.length;
+			textarea.selectionEnd = textarea.selectionStart;
+
+			this.selectedIndex = 0;
+		}, false);
+
+		for (var j = 0; j < wordfilters.length; j++) {
+			var option = document.createElement('option');
+			option.value = wordfilters[j][0];
+			option.textContent = wordfilters[j][1];
+			wordfiltersSelect.appendChild(option);
+		}
+
+		buttonBar.appendChild(wordfiltersSelect);
+	};
 })();
