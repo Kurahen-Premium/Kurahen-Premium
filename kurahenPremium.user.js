ddba11f52a14f804595ef01d80d8c76b423e94d5
kucanon <kucanon@users.noreply.github.com>
2014-06-07 18:27:56 +0200

    Release v1.2.1

--- kurahenPremium.user.js
+++ kurahenPremium.user.js
@@ -2,7 +2,7 @@
 // @name        Kurahen Premium
 // @namespace   karachan.org
 // @description Zestaw dodatkowych funkcji dla forum młodzieżowo-katolickiego
-// @version     1.2.0
+// @version     1.2.1
 
 // @match       *://*.karachan.org/*
 // @exclude     http://www.karachan.org/*/src/*
