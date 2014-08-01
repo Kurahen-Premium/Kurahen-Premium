f7cb1e2f14942c6265df55f6a38b1d37fd60fdf6
Ashpaner's lover <krzysiu.t1@gmail.com>
2014-08-01 20:24:29 +0200

    Added missing wordfilters #12

--- src/options.ts
+++ src/options.ts
@@ -27,7 +27,9 @@ var wordfilters = [
 	['#Lasoupeauxchoux', 'kapuśniaczek'],
 	['#homoś', 'pedał'],
 	['#korwinkrulempolski', 'kongres nowej prawicy'],
-	['#1%', 'groźny LEWAK wykryty']
+	['#1%', 'groźny LEWAK wykryty'],
+	['#mylittlefaggot', 'PRZYJAŹŃ JEST MAGIĄ'],
+	['hizume', 'Mała Księżniczka']
 ];
 var boardsWithId = ['b', 'fz', 'z'];
 var colors = [
